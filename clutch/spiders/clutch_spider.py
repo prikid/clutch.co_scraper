df7ba73bf500b22e88e18f9c9ba4f293b5545e02
prikid <sergey@prikid.ua>
2019-02-27 10:09:47 +0200

    Some changes

--- clutch/spiders/clutch_spider.py
+++ clutch/spiders/clutch_spider.py
@@ -3,20 +3,16 @@ import scrapy
 import w3lib.url
 import re
 
-
 # Columns I need: Agency name, website URL, contact email, employee range, location
+
 
 class ClutchSpiderSpider(scrapy.Spider):
     name = 'clutch_spider'
     allowed_domains = ['clutch.co']
 
-
     def start_requests(self):
         url = 'https://clutch.co/seo-firms'
-        # url = 'https://clutch.co/seo-firms?page=12'
-        # url = 'https://clutch.co/seo-firms?sort_by=field_pp_page_sponsor&field_pp_min_project_size=All&field_pp_hrly_rate_range=All&field_pp_size_people=All&field_pp_cs_small_biz=&field_pp_cs_midmarket=&field_pp_cs_enterprise=&client_focus=&field_pp_if_advertising=&field_pp_if_automotive=&field_pp_if_arts=&field_pp_if_bizservices=&field_pp_if_conproducts=&field_pp_if_education=&field_pp_if_natural_resources=&field_pp_if_finservices=&field_pp_if_gambling=&field_pp_if_gaming=&field_pp_if_government=&field_pp_if_healthcare=&field_pp_if_hospitality=&field_pp_if_it=&field_pp_if_legal=&field_pp_if_manufacturing=&field_pp_if_media=&field_pp_if_nonprofit=&field_pp_if_realestate=&field_pp_if_retail=&field_pp_if_telecom=&field_pp_if_transportation=&field_pp_if_utilities=&field_pp_if_other=&industry_focus=&field_pp_location_country_select=vn&field_pp_location_province=&field_pp_location_latlon_1%5Bpostal_code%5D=&field_pp_location_latlon_1%5Bsearch_distance%5D=100&field_pp_location_latlon_1%5Bsearch_units%5D=mile'
         yield scrapy.Request(url, callback=self.parse, errback=self.on_error)
-
 
     def parse(self, response):
         def get_email_from_script(script_text):
